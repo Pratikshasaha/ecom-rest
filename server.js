e6dbd20e860d74aca6434540f16c59f62e34b2d6
Pratikshasaha <prathikshasaha@rishfotech.com>
2026-04-09 09:51:29 +0530

    fix added

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 let users = [
-  { name: 'John Doe', email: 'john@example.com' },
+  { name: 'John Doe', email: 'john@example1.com' },
   { name: 'Jane Doe', email: 'jane@example.com' }
 ];
 
